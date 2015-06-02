298aec69e36579490880271075aab1626af05e6f
Jokerro <dan2629@yandex.ru>
2015-06-03 00:10:04 +0300

    InitialCommit + IDN + DB

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult ProcessImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> fileUpload)
+        {
+            foreach (var file in fileUpload)
+            {
+                if (file == null) continue;
+                string path = AppDomain.CurrentDomain.BaseDirectory + "UploadedFiles/";
+                string filename = System.IO.Path.GetFileName(file.FileName);
+                if (filename != null)
+                {
+                    file.SaveAs(System.IO.Path.Combine(path, filename));
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
