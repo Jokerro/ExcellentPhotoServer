9db07ba849e4784e4cb8911c5e4d79da7d63b5b3
Jokerro <dan2629@yandex.ru>
2015-06-04 15:01:34 +0300

    Upload and analyze photo implements

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -21,23 +21,68 @@ namespace WebApplication.Controllers
 
             return View();
         }
-
+        [AllowAnonymous]
         public ActionResult ProcessImage()
         {
             return View();
         }
+        public class ImageAnalyzer
+        {
+            private string fileName;
+            private double colorMark;
+            private double overalMark;
+            private const string PROCESSING_MODULE = "ExcellentPhoto\\ExcellentPhoto\\Release\\ExcellentPhoto.exe";
+            private const string COLOR_ANALYZE = " color ";
+            private const string FACE_ANALYZE = " face ";
+            private const string OVERAL_ANALYZE = " all ";
+            private string processingRequests;
+            
+            public void AddParameter(string analyzeParameter)
+            {
+                processingRequests += analyzeParameter;
+            }
+            public void OverallAnalyze(string imagePath)
+            {
+                ClearParametrs();
+                AddParameter(OVERAL_ANALYZE);
+                AddParameter(imagePath);
+                StartAnalyze();
+            }
+            private void ClearParametrs()
+            {
+                processingRequests = "";
+            }
+            private void StartAnalyze()
+            {
+                try
+                {
+                    System.Diagnostics.Process analyze = System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory + PROCESSING_MODULE, processingRequests);
+                    ClearParametrs();
+                    analyze.WaitForExit();
+                    analyze.Close();
+                }
+                catch (Exception e)
+                {
+                  
+                }
+            }
+        }
 
+        [AllowAnonymous]
         [HttpPost]
-        public ActionResult Index(IEnumerable<HttpPostedFileBase> fileUpload)
+        public ActionResult ImageProcessor(IEnumerable<HttpPostedFileBase> fileUpload)
         {
             foreach (var file in fileUpload)
             {
                 if (file == null) continue;
-                string path = AppDomain.CurrentDomain.BaseDirectory + "UploadedFiles/";
+                string path = AppDomain.CurrentDomain.BaseDirectory + "UploadedFiles\\";
                 string filename = System.IO.Path.GetFileName(file.FileName);
                 if (filename != null)
                 {
                     file.SaveAs(System.IO.Path.Combine(path, filename));
+                    string savedFilePath = file.FileName;
+                    ImageAnalyzer imageAnalyzer = new ImageAnalyzer();
+                    imageAnalyzer.OverallAnalyze(path + filename);
                 }
             }
 
