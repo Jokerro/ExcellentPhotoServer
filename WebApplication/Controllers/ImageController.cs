5c7bbafcd599bb156645e4fca24a58e62b98e6d6
Jokerro <dan2629@yandex.ru>
2015-06-12 05:48:04 +0300

    Release

---
+++ WebApplication/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace WebApplication.Controllers
+{
+    public class ImageController : ApiController
+    {
+        WebApplication.Models.Image[] products = new WebApplication.Models.Image[] 
+        { 
+            new WebApplication.Models.Image { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new WebApplication.Models.Image { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new WebApplication.Models.Image { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        public IEnumerable<WebApplication.Models.Image> GetAllImages()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetImage(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApplication/Controllers/UploadController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using System.Net.Http.Formatting;
+
+namespace WebApplication.Controllers
+{
+    public class UploadController : ApiController
+    {
+        private static WebApplication.Models.ImageAnalyzerHandler imageAnalyzer = new WebApplication.Models.ImageAnalyzerHandler();
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);            
+            
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+                
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    int fileNameLength = file.Headers.ContentDisposition.FileName.Length - 2;
+                    root += "//";
+                    root += file.Headers.ContentDisposition.FileName.Substring(1, fileNameLength);
+                    if (File.Exists(root))
+                    {
+                        File.Delete(root);
+                    }
+                    File.Move(file.LocalFileName, root);
+                }
+                imageAnalyzer.SetAnalyzedImage(root);
+                imageAnalyzer.OverallAnalysis();
+                return Request.CreateResponse(HttpStatusCode.OK, imageAnalyzer.GetImage().GetOverallRating());
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+    }
+}
